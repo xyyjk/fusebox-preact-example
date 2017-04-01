0310c931ab8e79397c385118ee1870edf7f801d6
xyyjk <xyyjk@126.com>
2017-04-01 16:26:49 +0800

    HMR

--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,13 @@ import App from './App.jsx';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
+
+// Set up HMR re-rendering.
+FuseBox.addPlugin({
+  hmrUpdate: ({ type, path, content }) => {
+    if (type === 'js') {
+      window.location.reload();
+      return true;
+    }
+  }
+});
